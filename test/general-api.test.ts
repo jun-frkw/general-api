bbed3cba48edcb24d15a9165f7ba8b37d0cbb427
jun.f <jun.furukawa.1120@gmail.com>
2024-12-01 14:29:13 +0900

    initial commit

---
+++ test/general-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GeneralApi from '../lib/general-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/general-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GeneralApi.GeneralApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
